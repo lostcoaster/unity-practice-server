85395e8eead129d2a3733addad9ddd4383bf2ec8
lostcoaster <fjy456@gmail.com>
2016-06-12 02:06:14 +0900

    move db place and add a deploy setting for django on server

---
+++ hubserver/deploy_settings.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#!python3
+# this file contains settings overrides that are meant to be used on real server
+# but don't abuse this file as most other files rely on the original setting file's content.
+
+
+__author__ = 'lostcoaster'
+
+from .settings import *
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     },
+# }
