73902ae64f00ec8233f34dc08652862db1573cba
lc <fjy456@gmail.com>
2017-05-24 20:05:08 +0900

    remove unrelated sites

--- shironeko/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DataportConfig(AppConfig):
-    name = 'shironeko'
--- shironeko/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-
-class ShironekoGatcha(models.Model):
-    time = models.DateTimeField(unique=True)
-    star = models.IntegerField()
